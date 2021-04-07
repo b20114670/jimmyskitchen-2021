9a0be0706d18bd4545e43a2b138f0752d62dc0e0
b20114670 <2016bryh@gmail.com>
2021-04-07 10:42:13 +0100

    first commit

---
+++ script-old.js
@@ -0,0 +1,271 @@
+let ifMouseEnter = "mouseenter";
+if (
+  /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  ifMouseEnter = "click";
+}
+
+// About Handle bookings Opening times Location Menus
+let hamburger = document.querySelector("#navbar-hamburger");
+let hamburgerLinks = [];
+if (document.querySelector("#home-page")) {
+  hamburgerLinks = [
+    "Menus",
+    "Location",
+    "Opening times",
+    "Handle bookings",
+    "About"
+  ];
+} 
+if (document.querySelector("#about-page")) {
+  hamburgerLinks = [
+    "Restaurant",
+    "Staff",
+    "Handle bookings",
+    "About"
+  ];
+} 
+if (document.querySelector("#handle-bookings-page")) {
+  hamburgerLinks = [
+    "Make a booking",
+    "Your bookings",
+    "Handle bookings",
+    "About"
+  ];
+} 
+
+let mobileNav = document.querySelector("#mobile-nav");
+hamburger.addEventListener("click", (e) => {
+  mobileNav.classList.toggle("display");
+  mobileNav.innerHTML = "";
+  hamburgerLinks.forEach((link) => {
+    mobileNav.innerHTML += `<a class="animated fadeIn navbar-item" href="#${link
+      .replace(/ /g, "-")
+      .toLowerCase()}">${link}</a>`;
+    mobileNav.querySelectorAll("a").forEach((a) => {
+      a.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("HI"); 
+        console.log(JSON.stringify(e.target.dataset)); 
+        let element = document.getElementById(e.target.href.split("#")[1]);
+            element.scrollIntoView({
+              behavior: "smooth",
+              block: "end",
+              inline: "nearest"
+            });
+        });
+      });
+  });
+});
+
+document.querySelectorAll(".navbar-item").forEach((a) => {
+  a.addEventListener("click", (e) => {
+    if (!e.target.classList.value.includes("fa")) {
+      e.preventDefault();
+      // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+      let element = document.getElementById(e.target.href.split("#")[1]);
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest"
+      });
+    }
+  });
+});
+
+let imagesPath = "./images/";
+let startersTab = document.querySelector("#starters-tab button");
+let mainsTab = document.querySelector("#mains-tab button");
+let desertsTab = document.querySelector("#deserts-tab button");
+let tabs = [startersTab, mainsTab, desertsTab];
+let menu = document.querySelector("#menu");
+let startersHTML = `
+        <div class="animated fadeIn">
+            <div class="heading">
+                <h1 class="title">Starters</h1>
+                </div> 
+                <div class="food-item" id="food-item">
+                    <div class="name-and-price columns">
+                        <div class="column name"><strong>Food name</strong></div>
+                                <div class="column price is-pulled-right"><strong>£20</strong></div>
+                                </div>                    
+                                <div class="description">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, libero?</p>
+                                    </div> 
+                        </div>
+                        <div class="food-item" id="food-item">
+                            <div class="name-and-price columns">
+                                <div class="column name" id="menu-name"><strong>Food name</strong></div>
+                                <div class="column price is-pulled-right"><strong>£20</strong></div>
+                                </div>                    
+                                <div class="description">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, libero?</p>
+                                    </div> 
+                        </div>
+                        <div class="food-item" id="food-item">
+                            <div class="name-and-price columns">
+                                <div class="column name" id="menu-name"><strong>Food name</strong></div>
+                                <div class="column price is-pulled-right"><strong>£20</strong></div>
+                            </div>                    
+                            <div class="description">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, libero?</p>
+                                </div> 
+                        </div>
+                        </div>                
+                        `;
+let desertsHTML = `
+        <div class="animated fadeIn">
+            <div class="heading">
+                <h1 class="title">Deserts</h1>
+                </div> 
+                <div class="food-item" id="food-item">
+                    <div class="name-and-price columns">
+                        <div class="column name" id="menu-name"><strong>Food name</strong></div>
+                        <div class="column price is-pulled-right"><strong>£20</strong></div>
+                        </div>                    
+                        <div class="description">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, libero?</p>
+                            </div> 
+                            </div>
+                            <div class="food-item" id="food-item">
+                                <div class="name-and-price columns">
+                                    <div class="column name" id="menu-name"><strong>Food name</strong></div>
+                                    <div class="column price is-pulled-right"><strong>£20</strong></div>
+                                    </div>                    
+                                    <div class="description">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, libero?</p>
+                                        </div> 
+                                        </div>
+                                        <div class="food-item" id="food-item">
+                                            <div class="name-and-price columns">
+                                                <div class="column name" id="menu-name"><strong>Food name</strong></div>
+                                <div class="column price is-pulled-right"><strong>£20</strong></div>
+                                </div>                    
+                                <div class="description">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, libero?</p>
+                            </div> 
+                            </div>
+                    </div>                
+                    `;
+let mainsHTML = `
+        <div class="animated fadeIn">
+            <div class="heading">
+                <h1 class="title">Mains</h1>
+            </div> 
+            <div class="food-item" id="food-item">
+                <div class="name-and-price columns">
+                    <div class="column name"><strong>Food name</strong></div>
+                    <div class="column price is-pulled-right"><strong>£20</strong></div>
+                    </div>                    
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, libero?</p>
+                        </div> 
+                        </div>
+                        <div class="food-item" id="food-item">
+                            <div class="name-and-price columns">
+                                <div class="column name" id="menu-name"><strong>Food name</strong></div>
+                                <div class="column price is-pulled-right"><strong>£20</strong></div>
+                                </div>                    
+                                <div class="description">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, libero?</p>
+                                    </div> 
+            </div>
+            <div class="food-item" id="food-item">
+                <div class="name-and-price columns">
+                    <div class="column name" id="menu-name"><strong>Food name starter</strong></div>
+                    <div class="column price is-pulled-right"><strong>£20</strong></div>
+                    </div>                    
+                    <div class="description">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, libero?</p>
+                    </div> 
+                    </div>
+                    </div>                
+                    `;
+
+if (menu) {
+  menu.innerHTML = startersHTML;
+}
+
+tabs.forEach((tab) => {
+  if (tab) {
+    tab.addEventListener(ifMouseEnter, (e) => {
+      console.log(e.target.innerText);
+      // display appropriate menu
+      // note: #menu-name is unlikely to be used
+      // note: I do not like using classes in JS but in this it seems most appropriate
+      switch (e.target.innerText.toLowerCase()) {
+        case "starters":
+          menu.innerHTML = startersHTML;
+          handleMenuImageOnItemHover(); // note comfortable with this function's name
+          break;
+        case "mains":
+          menu.innerHTML = mainsHTML;
+          handleMenuImageOnItemHover(); // note comfortable with this function's name
+          break;
+        case "deserts":
+          menu.innerHTML = desertsHTML;
+          handleMenuImageOnItemHover(); // note comfortable with this function's name
+          break;
+        default:
+          // menu.innerHTML = "def";
+          return;
+      }
+    });
+  }
+});
+handleMenuImageOnItemHover(); // note comfortable with this function's name
+
+function handleMenuImageOnItemHover() {
+  let menuFoodItems = document.querySelectorAll(".menu .food-item");
+  if (menuFoodItems) {
+    menuFoodItems.forEach((menuFoodItems) =>
+      menuFoodItems.addEventListener(ifMouseEnter, (e) => {
+        if (e.target.querySelector(".name strong")) {
+          let menuName = e.target
+            .querySelector(".name strong")
+            .innerText.replace(/ /g, "-")
+            .toLowerCase();
+          if (menuName) {
+            // decides to place the HTML instead of just changing image source because I have yet to decide how to display the image based on responsiveness
+            let menuFoodItemImage = document.querySelector(
+              "#menu-food-item-image"
+            );
+            if (menuFoodItemImage) {
+              menuFoodItemImage.innerHTML = `<div class="animated fadeIn"><img src="${imagesPath}${menuName}.jpeg" /></div>`;
+            }
+          }
+        }
+      })
+    );
+  }
+}
+
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 20000); // Change image every 2 seconds
+}
+
